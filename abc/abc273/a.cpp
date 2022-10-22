7e956a76d978b0e618cfcd62ced846e784281b44
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2022-10-22 21:26:58 +0900

    add: abc273 a

---
+++ abc/abc273/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, ans;
+  cin >> n;
+  ans = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    ans *= i;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
